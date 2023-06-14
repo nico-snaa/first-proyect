a34b02855706c8a211f97b9a55038513ceac7cab
nico-snaa <123990728+nico-snaa@users.noreply.github.com>
2023-06-14 16:40:09 -0500

    script.js

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-var cartItems = [];
-
-function addToCart(productId) {
-  cartItems.push(productId);
-  updateCart();
-}
-
-function updateCart() {
-  var cartList = document.getElementById('cart-items');
-  cartList.innerHTML = '';
-
-  for (var i = 0; i < cartItems.length; i++) {
-    var listItem = document.createElement('li');
-    listItem.textContent = 'Producto ' + cartItems[i];
-    cartList.appendChild(listItem);
-  }
-}
-
-function checkout() {
-  // Lógica de pago
-  alert('Gracias por tu compra!');
-  cartItems = [];
-  updateCart();
-}
+window.addEventListener('DOMContentLoaded', (event) => {
+    const buyButtons = document.querySelectorAll('.buy-button');
+    buyButtons.forEach((button) => {
+      button.addEventListener('click', () => {
+        alert('Producto comprado');
+      });
+    });
+  });
+  
